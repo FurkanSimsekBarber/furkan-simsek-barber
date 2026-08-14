4f0f10e1ae90fa9cdf29967459cd42bd8772443a
FurkanSimsekBarber <furkansimsek993@gmail.com>
2026-08-15 02:59:06 +0300

    radio.js

--- js/radio.js
+++ js/radio.js
@@ -19,7 +19,7 @@ document.addEventListener("DOMContentLoaded", () => {
        RADYO
        ========================================= */
 
-   cconst RADIO_STREAM_URL = "http://yayin2.canliyayin.org:7424/";
+    const RADIO_STREAM_URL = "http://yayin2.canliyayin.org:7424/";
 
 
     /* =========================================
